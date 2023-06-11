68dc7684fa5561a67d05633c34e4332d6c069401
Stephen Bondurant <stephenbondurant2014@gmail.com>
2023-06-11 15:01:03 -0500

    Models mostly done

---
+++ models/Thought.js
@@ -0,0 +1,27 @@
+const { Schema, Types } = require("mongoose");
+const Reaction = require("./reaction");
+
+const thoughtSchema = new Schema({
+  thoughtText: {
+    type: String,
+    required: true,
+    minlength: 1,
+    maxlength: 280,
+  },
+  createdAt: {
+    tpye: Date,
+    default: Date.now,
+  },
+  username: {
+    type: String,
+    required: true,
+  },
+  reactions: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Reaction",
+    },
+  ],
+});
+
+module.exports = Thought;
---
+++ models/index.js
@@ -0,0 +1,5 @@
+const User = require("./User");
+const Thought = require("./Thought");
+reactionSchema = require("./reaction");
+
+module.exports = { User, Thought, reactionSchema };
