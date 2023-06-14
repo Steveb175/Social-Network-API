7fa170586631594b991185dc508738261ee2dfee
Stephen Bondurant <stephenbondurant2014@gmail.com>
2023-06-14 11:44:26 -0500

    Input logic for api routes

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -1,1 +1,23 @@
+const router = require("express").Router();
+const {
+  getThoughts,
+  getOneThought,
+  createThought,
+  deleteThought,
+  addReaction,
+  removeReaction,
+} = require("../../controllers/thoughtController");
 
+// Create thought
+router.route("/").get(getThoughts).post(createThought);
+
+// Delete thought
+router.route("/:thoughtId").get(getOneThought).delete(deleteThought);
+
+// Create reaction
+router.route("/:thoughtId/reactions").post(addReaction);
+
+// Delete reaction
+router.route("/:thoughtId/reactions/:reactionId").delete(removeReaction);
+
+module.exports = router;
--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -1,1 +1,27 @@
+const router = require("express").Router();
+const {
+  getUsers,
+  getOneUser,
+  createUser,
+  updateUser,
+  deleteUser,
+  addFriend,
+  removeFriend,
+} = require("../../controllers/userController");
 
+// Create user
+router.route("/").get(getUsers).post(createUser);
+
+// Delete user
+router.route("/:userId").get(getOneUser).delete(deleteUser);
+
+// Update user
+router.route("/:userId").get(getOneUser).put(updateUser).delete(deleteUser);
+
+// Add friend
+router.route("/:userId/friends").post(addFriend);
+
+// Remove friend
+router.route("/:userId/friends/:friendId").delete(removeFriend);
+
+module.exports = router;
