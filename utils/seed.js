c904b2362e4fada1d610f7370384409e5df47e3b
Stephen Bondurant <stephenbondurant2014@gmail.com>
2023-06-14 12:32:07 -0500

    Finished logic in seed.js

--- utils/seed.js
+++ utils/seed.js
@@ -1,2 +1,24 @@
 const connection = require("../config/connection");
 const { Thought, User } = require("../models");
+const { getUsername } = require("./data");
+
+connection.on("error", (err) => err);
+
+connection.once("open", async () => {
+  console.log("connected");
+
+  //Drop existing data
+  await Thought.deleteMany({});
+  await User.deleteMany({});
+
+  // Get username from helper function
+  const users = getUsername(11);
+
+  //Add users to the collection
+  await User.collection.insertMany(users);
+
+  // Log seed data
+  console.table(users);
+  console.info("Seeding complete!");
+  process.exit(0);
+});
